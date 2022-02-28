60ef82d5325614b7a65715efb3bcc2f30b8d0370
Jack-Balcombe <18062@wc.school.nz>
2022-03-01 10:48:25 +1300

    Replaced the 'Hello World' testing page created by app.py with a program which uses the HTML templates to render the three web pages

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def render_homepage():
     """
 
-    :return: Returns the string "Hello, World!"
+    :return: TBA
     """
-    return 'Hello, World!'
+    return render_template('home.html')
+
+
+@app.route('/menu')
+def render_menu_page():
+    """
+
+    :return: TBA
+    """
+    return render_template('menu.html')
+
+
+@app.route('/contact')
+def render_contact_page():
+    """
+
+    :return: TBA
+    """
+    return render_template('contact.html')
 
 
 app.run(host='0.0.0.0')
