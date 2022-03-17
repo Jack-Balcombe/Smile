12908a7c9f6e519c9951b83d7e97643ecfef48f8
Jack-Balcombe <18062@wc.school.nz>
2022-03-17 14:00:28 +1300

    Make app.py connect to database and display product info on a table when menu page is opened

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request, session
+import sqlite3
+from sqlite3 import Error
+
+DB_NAME = "smile.db"
 
 app = Flask(__name__)
+
+# creates a connection to the database
+# inputs: database file
+# outputs: the connection to the db or none.
+def create_connection(db_file):
+    """ create a connection to the sqlite db"""
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 
 @app.route('/')
@@ -13,12 +29,21 @@ def render_homepage():
 
 
 @app.route('/menu')
+@app.route('/menu/<products>')
 def render_menu_page():
     """
 
     :return: TBA
     """
-    return render_template('menu.html')
+    con = create_connection(DB_NAME)
+    query = "SELECT name, description, volume, price, image FROM product"
+
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
+    con.close()
+
+    return render_template('menu.html', products=product_list)
 
 
 @app.route('/contact')
